7d748d0a214a0b4ba370be2142d55552b885adc5
Manik <itsmanikofficial@gmail.com>
2023-06-01 16:16:50 +0530

    add script to calculate sgpa for chem cycle

---
+++ projects/gpa/script.js
@@ -0,0 +1,24 @@
+let calcBtn = document.querySelector(".calcBtn");
+let result = document.querySelector(".sgpaResult");
+
+calcBtn.addEventListener("click", function () {
+  let mat = document.querySelector("#sub1").value;
+  let chem = document.querySelector("#sub2").value;
+  let ced = document.querySelector("#sub3").value;
+  let pws = document.querySelector("#sub4").value;
+  let ico = document.querySelector("#sub5").value;
+  let sfh = document.querySelector("#sub6").value;
+  let plc = document.querySelector("#sub7").value;
+  let esc = document.querySelector("#sub8").value;
+  mat = Math.ceil(Number(mat) / 10) * 4;
+  chem = Math.ceil(Number(chem) / 10) * 4;
+  ced = Math.ceil(Number(ced) / 10) * 3;
+  pws = Math.ceil(Number(pws) / 10);
+  ico = Math.ceil(Number(ico) / 10);
+  sfh = Math.ceil(Number(sfh) / 10);
+  plc = Math.ceil(Number(plc) / 10) * 3;
+  esc = Math.ceil(Number(esc) / 10) * 3;
+  let scoreSum = mat + chem + ced + pws + ico + sfh + plc + esc;
+  let sgpa = scoreSum / 20;
+  result.innerHTML = sgpa;
+});
